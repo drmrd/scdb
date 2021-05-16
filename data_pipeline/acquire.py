0a7658114ed699116eaeb33379366797a5b85fcf
Dan Moore <hello@danielmoore.xyz>
2021-05-16 16:57:19 -0400

    Update download file paths for raw data

--- data_pipeline/acquire.py
+++ data_pipeline/acquire.py
@@ -3,6 +3,7 @@ import io
 import zipfile
 
 from pathlib import Path
+from typing import List
 
 import git
 import requests
@@ -19,29 +20,50 @@ _REPO_ROOT = Path(
 _DATA_PATH = _REPO_ROOT / 'data'
 
 
-def acquire_scdb_datasets(destination: Path = _DATA_PATH / 'raw' / 'scdb'):
-    for unit_of_analysis in ['case', 'justice']:
-        acquire_scdb_dataset(
-            scdb_version='Legacy_06',
-            dataset_version=f'{unit_of_analysis}Centered_Citation',
-            data_dir=destination
+def acquire_all_scdb_datasets_by_version(
+    scdb_versions: List[str] = ('Legacy_06', '2020_01'),
+    destination: Path = _DATA_PATH / 'raw'
+) -> None:
+    legacy_versions = [version for version in scdb_versions if version.startswith('Legacy')]
+    modern_versions = list(set(scdb_versions) - set(legacy_versions))
+
+    for legacy_version in legacy_versions:
+        acquire_scdb_datasets(
+            scdb_version=legacy_version,
+            units_of_analysis=['case', 'justice'],
+            record_granularities=['Citation'],
+            destination=destination / legacy_version
         )
-        for record_granularity in ['Citation', 'Docket', 'LegalProvision', 'Vote']:
+
+    for modern_version in modern_versions:
+        acquire_scdb_datasets(
+            scdb_version=modern_version,
+            units_of_analysis=['case', 'justice'],
+            record_granularities=['Citation', 'Docket', 'LegalProvision', 'Vote'],
+            destination=destination / modern_version
+        )
+
+
+def acquire_scdb_datasets(scdb_version, units_of_analysis, record_granularities, destination):
+    for unit_of_analysis in units_of_analysis:
+        for record_granularity in record_granularities:
             acquire_scdb_dataset(
-                scdb_version='2020_01',
+                scdb_version=scdb_version,
                 dataset_version=f'{unit_of_analysis}Centered_{record_granularity}',
-                data_dir=destination
+                destination=destination
             )
 
 
-def acquire_scdb_dataset(scdb_version, dataset_version, data_dir):
-    data_dir.mkdir(parents=True, exist_ok=True)
-    file_name = f'SCDB_{scdb_version}_{dataset_version}.sav'
-    url = f'http://supremecourtdatabase.org/_brickFiles/{scdb_version}/{file_name}.zip'
-    local_file = Path(data_dir) / file_name
+def acquire_scdb_dataset(scdb_version, dataset_version, destination):
+    source_file_name = f'SCDB_{scdb_version}_{dataset_version}.sav'
+    url = f'http://supremecourtdatabase.org/_brickFiles/{scdb_version}/{source_file_name}.zip'
 
-    if not local_file.exists():
-        download_and_extract_zip(url, data_dir)
+    destination_file_path = destination / f'{dataset_version}.sav'
+
+    if not destination_file_path.exists():
+        destination_file_path.parent.mkdir(parents=True, exist_ok=True)
+        print(destination_file_path)
+        download_and_extract_zip(url, destination_file_path.parent)
 
 
 def download_and_extract_zip(url, destination_dir):
@@ -51,4 +73,4 @@ def download_and_extract_zip(url, destination_dir):
 
 
 if __name__ == '__main__':
-    acquire_scdb_datasets()
+    acquire_all_scdb_datasets_by_version()
--- tests/test_acquire.py
+++ tests/test_acquire.py
@@ -1,11 +1,13 @@
+import io
 import zipfile
 from unittest.mock import Mock, patch
 
 import pytest
 
-from data_pipeline.acquire import acquire_scdb_datasets, SCDB_DATASET_URL_TEMPLATE
+from data_pipeline.acquire import acquire_all_scdb_datasets_by_version, SCDB_DATASET_URL_TEMPLATE
 
 
+DESIRED_VERSIONS = ['Legacy_999', 'Legacy_3000', 'some_other_version', 'and_another', 'and_one_more_for_good_measure']
 DESIRED_DATASET_URLS = [
     SCDB_DATASET_URL_TEMPLATE.format(
         scdb_version=version,
@@ -13,7 +15,7 @@ DESIRED_DATASET_URLS = [
         record_granularity=granularity,
         file_extension=extension
     )
-    for version in ['2020_01', 'Legacy_06']
+    for version in DESIRED_VERSIONS
     for unit in ['case', 'justice']
     for granularity in ['Citation', 'Docket', 'LegalProvision', 'Vote']
     for extension in ['.sav.zip']
@@ -24,6 +26,19 @@ DESIRED_FILE_NAMES = [
     url.rsplit('/', maxsplit=1)[-1]
     for url in DESIRED_DATASET_URLS
 ]
+
+
+class ZipFileDouble:
+    def __init__(self):
+        self.provided_files = []
+        self.extraction_paths = []
+
+    def __call__(self, file_object, *_, **__):
+        self.provided_files.append(file_object)
+        return self
+
+    def extractall(self, path, *_, **__):
+        self.extraction_paths.append(path)
 
 
 @pytest.fixture(autouse=True)
@@ -38,30 +53,48 @@ def responses():
 def test_requests_all_desired_datasets(responses, monkeypatch, tmp_path):
     monkeypatch.setattr(zipfile, 'ZipFile', Mock())
 
-    acquire_scdb_datasets(tmp_path)
+    acquire_all_scdb_datasets_by_version(scdb_versions=DESIRED_VERSIONS, destination=tmp_path)
 
     requested_urls = [call.response.url for call in responses.calls]
     assert sorted(requested_urls) == sorted(DESIRED_DATASET_URLS)
 
 
-def test_attempts_to_unzip_all_downloaded_files(monkeypatch, tmp_path):
-    zip_file_mock = Mock()
-    monkeypatch.setattr(zipfile, 'ZipFile', zip_file_mock)
+def test_attempts_to_create_zip_file_objects_from_all_downloaded_files(monkeypatch, tmp_path):
+    zip_file_double = ZipFileDouble()
+    monkeypatch.setattr(zipfile, 'ZipFile', zip_file_double)
 
-    acquire_scdb_datasets(tmp_path)
+    acquire_all_scdb_datasets_by_version(scdb_versions=DESIRED_VERSIONS, destination=tmp_path)
 
     requested_file_names = []
-    for zip_file_mock_call in zip_file_mock.call_args_list:
-        assert len(zip_file_mock_call.args) == 1
-        zip_file = zip_file_mock_call.args[0]
-        zip_file.seek(0)
-        requested_file_names.append(zip_file.read().decode('utf-8'))
-
+    for provided_file in zip_file_double.provided_files:
+        assert isinstance(provided_file, io.BytesIO)
+        provided_file.seek(0)
+        requested_file_names.append(provided_file.read().decode('utf-8'))
     assert sorted(requested_file_names) == sorted(DESIRED_FILE_NAMES)
+
+
+def test_attempts_to_unzip_all_downloaded_files(monkeypatch, tmp_path):
+    desired_legacy_version_count = sum(version.startswith('Legacy') for version in DESIRED_VERSIONS)
+    desired_modern_version_count = len(DESIRED_VERSIONS) - desired_legacy_version_count
+    expected_files_per_legacy_version = 2
+    expected_files_per_modern_version = 8
+
+    zip_file_double = ZipFileDouble()
+    monkeypatch.setattr(zipfile, 'ZipFile', zip_file_double)
+
+    acquire_all_scdb_datasets_by_version(scdb_versions=DESIRED_VERSIONS, destination=tmp_path)
+
+    assert len(zip_file_double.provided_files) == len(zip_file_double.extraction_paths)
+
+    actual_legacy_file_count = sum('Legacy' in str(path) for path in zip_file_double.extraction_paths)
+    actual_modern_file_count = len(zip_file_double.extraction_paths) - actual_legacy_file_count
+
+    assert actual_legacy_file_count == desired_legacy_version_count * expected_files_per_legacy_version
+    assert actual_modern_file_count == desired_modern_version_count * expected_files_per_modern_version
 
 
 @patch('pathlib.Path.exists', lambda *_, **__: True)
 def test_does_not_request_datasets_that_have_already_been_downloaded(tmp_path, responses):
     responses.reset()
 
-    acquire_scdb_datasets(tmp_path)
+    acquire_all_scdb_datasets_by_version(destination=tmp_path)
