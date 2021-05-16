fd059a7fd76290e17ba7efdb34af9444951ab602
Dan Moore <hello@danielmoore.xyz>
2021-05-16 15:06:46 -0400

    Prefer the new experimental pandas StringDtype over object

--- data_pipeline/featherize.py
+++ data_pipeline/featherize.py
@@ -20,13 +20,14 @@ def featherize_scdb_datasets():
         scdb_feather_path = interim_data_dir / f'{scdb_sav_path.stem}.feather'
         scdb_dataset = scdb_sav_to_dataframe(scdb_sav_path)
         categorical_columns = scdb_dataset.select_dtypes(include='category').columns
+        object_columns = scdb_dataset.select_dtypes(include='object').columns
         (scdb_dataset
-             .pipe(lambda df: df.astype({column: str for column in categorical_columns}))
+             .astype({column: str for column in categorical_columns})
              .pipe(lambda df: df.fillna({
-                 column_name: 'MISSING_VALUE'
-                 for column_name in df.select_dtypes(include='object').columns
+                 column_name: 'MISSING_VALUE' for column_name in object_columns
              }))
-             .pipe(lambda df: df.astype({column: 'category' for column in categorical_columns}))
+             .astype({column: 'category' for column in categorical_columns}
+                     | {column: 'string' for column in object_columns})
              .to_feather(scdb_feather_path))
 
 
